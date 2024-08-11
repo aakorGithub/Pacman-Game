ea72562b0a969c45b736f8ac514d6c1be2ee2680
Anthony Akor <anthonyakor70@gmail.com>
2024-08-11 14:52:07 -0700

    Added src folder and updated other files

--- src/Game.js
+++ src/Game.js
@@ -5,8 +5,9 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext('2d');
 const tileMap = new TileMap(tileSize);
 function gameloop(){
-  tileMap.draw();
+  tileMap.draw(ctx);
 
 }
 
+tileMap.setCanvasSize(canvas);
 setInterval(gameloop, 1000 / 75);
--- src/TileMap.js
+++ src/TileMap.js
@@ -1,22 +1,54 @@
 export default class TileMap{
   constructor(tileSize) {
     this.tileSize = tileSize;
+
+    this.yellowDot = new Image()
+    this.yellowDot.src = "../images/yellowdot.png";
+
+    this.wall = new Image()
+    this.wall.src = "../images/wall.png";
   }
 
   map = [
       [1,1,1,1,1,1,1,1,1,1,1,1,1],
       [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
-      [1,0,0,0,0,0,0,0,0,0,0,0,1],
+      [1,0,1,1,1,1,1,1,1,0,1,0,1],
+      [1,0,1,0,0,0,0,0,0,0,1,0,1],
+      [1,0,1,0,1,1,1,0,1,0,1,0,1],
+      [1,0,1,0,1,0,0,0,1,0,1,0,1],
+      [1,0,1,0,1,0,1,0,1,0,1,0,1],
+      [1,0,1,0,1,0,1,0,0,0,1,0,1],
       [1,0,0,0,0,0,0,0,0,0,0,0,1],
       [1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
     
-  draw() {
-    // console.log("draw");
+  
+  draw(ctx) {
+    for (let row=0; row < this.map.length; row++){
+        for (let column = 0; column < this.map[row].length; column++) {
+            let tile = this.map[row][column];
+            if (tile === 1) {
+              this.#drawall(ctx, column, row, this.tileSize);
+            }
+            else if (tile ===0){
+                this.#drawDot(ctx, column, row, this.tileSize);
+            }
+        }
+    }  
+
+  }
+
+  #drawDot(ctx,column,row,size){
+      ctx.drawImage(this.yellowDot,column* this.tileSize,row* this.tileSize, size,size);
+  }
+
+  #drawall(ctx,column,row,size){
+      ctx.drawImage(this.wall,column*this.tileSize,row * this.tileSize, size,size);
+  }
+
+
+  setCanvasSize(canvas){
+      canvas.width = this.map[0].length * this.tileSize;
+      canvas.height = this.map.length * this.tileSize;
   }
 }
